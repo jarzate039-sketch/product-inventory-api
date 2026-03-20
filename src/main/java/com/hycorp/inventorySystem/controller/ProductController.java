1702073a9c14d0bc9afce76607fa458817bb84db
jarzate039-sketch <jarzate039@gmail.com>
2026-03-20 10:00:38 -0600

    feature: creation of patch method

--- src/main/java/com/hycorp/inventorySystem/controller/ProductController.java
+++ src/main/java/com/hycorp/inventorySystem/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hycorp.inventorySystem.dto.ProductRequestDTO;
 import com.hycorp.inventorySystem.dto.ProductResponseDTO;
+import com.hycorp.inventorySystem.dto.StockRequestDTO;
+import com.hycorp.inventorySystem.dto.StockResponseDTO;
 import com.hycorp.inventorySystem.service.ProductService;
 
 import lombok.AllArgsConstructor;
@@ -70,5 +73,9 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @PatchMapping("/{id}/stock")
+    public ResponseEntity<StockResponseDTO> updateStock(@PathVariable(value = "id") UUID id, @RequestBody StockRequestDTO stockDTO){
+        return ResponseEntity.status(HttpStatus.OK).body(service.updateStock(id, stockDTO));
+    }
 
 }
---
+++ src/main/java/com/hycorp/inventorySystem/dto/StockResponseDTO.java
@@ -0,0 +1,30 @@
+package com.hycorp.inventorySystem.dto;
+
+import java.util.UUID;
+
+import com.hycorp.inventorySystem.constants.StockOperationEnum;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Schema(name="StockResponseDTO", description = "This is the DTO for the stock response")
+public class StockResponseDTO {
+
+    @Schema(description = "The ID of the product", example = "123e4567-e89b-12d3-a456-426655440000")
+    private UUID id;
+    @Schema(description = "The name of the product", example = "Coca-Cola")
+    private String name;
+    @Schema(description = "The actual stoc", example = "110")
+    private Integer stock;
+    @Schema(description = "The previous stock", example = "100")
+    private Integer previousStock;
+    @Schema(description = "The type of the operation", example = "ADD")
+    private StockOperationEnum operation;
+    @Schema(description = "The quantity of the product to add or substract", example = "10")
+    private Integer quantity;
+
+    
+}
